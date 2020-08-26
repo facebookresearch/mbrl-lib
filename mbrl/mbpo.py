79185852ebf8533f736d60a09d066fc68ef4c563
Luis Pineda <luisen.p@gmail.com>
2020-08-26 18:57:54 -0400

    added separate models for logger and sac outputs

--- mbrl/mbpo.py
+++ mbrl/mbpo.py
@@ -1,5 +1,5 @@
 import os
-from typing import Callable, Tuple
+from typing import Callable
 
 import gym
 import hydra.utils
@@ -10,6 +10,35 @@ import torch
 
 import mbrl.models as models
 import mbrl.replay_buffer as replay_buffer
+
+
+MODEL_LOG_FORMAT = [
+    ("calls", "E", "int"),
+    ("step", "S", "int"),
+    ("train_dataset_size", "TD", "int"),
+    ("val_dataset_size", "VD", "int"),
+    ("model_loss", "MLOSS", "float"),
+    ("model_val_score", "MVSCORE", "float"),
+]
+
+SAC_EVAL_LOG_FORMAT = [
+    ("episode", "E", "int"),
+    ("step", "S", "int"),
+    ("episode_reward", "R", "float"),
+]
+
+SAC_TRAIN_LOG_FORMAT = [
+    ("episode", "E", "int"),
+    ("step", "S", "int"),
+    ("episode_reward", "R", "float"),
+    ("duration", "D", "time"),
+    ("batch_reward", "BR", "float"),
+    ("actor_loss", "ALOSS", "float"),
+    ("critic_loss", "CLOSS", "float"),
+    ("alpha_loss", "TLOSS", "float"),
+    ("alpha_value", "TVAL", "float"),
+    ("actor_entropy", "AENT", "float"),
+]
 
 
 def collect_random_trajectories(
@@ -88,8 +117,20 @@ def train(
     agent = hydra.utils.instantiate(cfg.agent)
 
     work_dir = os.getcwd()
-    logger = pytorch_sac.Logger(
-        work_dir, save_tb=cfg.log_save_tb, log_frequency=cfg.log_frequency, agent="sac"
+    model_logger = pytorch_sac.Logger(
+        work_dir,
+        save_tb=cfg.log_save_tb,
+        log_frequency=cfg.log_frequency_sac,
+        agent="model",
+        train_format=MODEL_LOG_FORMAT,
+        eval_format=MODEL_LOG_FORMAT,
+    )
+    sac_logger = pytorch_sac.Logger(
+        work_dir,
+        save_tb=cfg.log_save_tb,
+        log_frequency=cfg.log_frequency_sac,
+        train_format=SAC_TRAIN_LOG_FORMAT,
+        eval_format=SAC_EVAL_LOG_FORMAT,
     )
 
     rng = np.random.RandomState(cfg.seed)
@@ -130,6 +171,14 @@ def train(
     updates_made = 0
     env_steps = 0
     model_env = models.ModelEnv(env, ensemble, termination_fn)
+    model_trainer = models.EnsembleTrainer(
+        ensemble,
+        device,
+        env_dataset_train,
+        dataset_val=env_dataset_val,
+        logger=model_logger,
+        log_frequency=cfg.log_frequency_model,
+    )
     for epoch in range(cfg.num_epochs):
         obs = env.reset()
         done = False
@@ -145,13 +194,13 @@ def train(
 
             # --------------- Model Training -----------------
             if env_steps % cfg.freq_train_dyn_model == 0:
-                train_loss, val_score = models.train_dyn_ensemble(
-                    ensemble,
-                    env_dataset_train,
-                    device,
-                    dataset_val=env_dataset_val,
-                    patience=cfg.patience,
+                model_logger.log(
+                    "train/train_dataset_size", env_dataset_train.num_stored, env_steps
                 )
+                model_logger.log(
+                    "train/val_dataset_size", env_dataset_val.num_stored, env_steps
+                )
+                model_trainer.train(patience=cfg.patience)
 
             # --------------- Agent Training -----------------
             sac_buffer = pytorch_sac.ReplayBuffer(
@@ -169,9 +218,9 @@ def train(
                 )
 
                 for _ in range(cfg.num_sac_updates_per_rollout):
-                    agent.update(sac_buffer, logger, updates_made)
+                    agent.update(sac_buffer, sac_logger, updates_made)
                     updates_made += 1
 
-            logger.dump(updates_made, save=True)
+            sac_logger.dump(updates_made, save=True)
 
             env_steps += 1
