e899b8edf6b80817376e2f7f50826a4c7fafa17c
Luis Pineda <luisen.p@gmail.com>
2020-10-19 12:48:30 -0400

    added wrapper to use original pets halfcheetah code

--- mbrl/env/__init__.py
+++ mbrl/env/__init__.py
@@ -1,3 +1,8 @@
-from . import cartpole_continuous, pets_cartpole, reward_fns, termination_fns
+from . import cartpole_continuous, halfcheetah_pets_wrapper, reward_fns, termination_fns
 
-__all__ = ["cartpole_continuous", "pets_cartpole", "reward_fns", "termination_fns"]
+__all__ = [
+    "cartpole_continuous",
+    "halfcheetah_pets_wrapper",
+    "reward_fns",
+    "termination_fns",
+]
