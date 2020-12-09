74be7a6f7ec111eb2617df1eb16a77ad2eb4dce6
Luis Pineda <luisen.p@gmail.com>
2020-12-09 12:31:57 -0500

    replaced use of pytorch_sac's logger on mbpo with mbrl.logger

--- mbrl/mbpo.py
+++ mbrl/mbpo.py
@@ -5,10 +5,10 @@ import gym
 import hydra.utils
 import numpy as np
 import omegaconf
-import pytorch_sac
 import pytorch_sac.utils
 import torch
 
+import mbrl.logger
 import mbrl.models as models
 import mbrl.planning
 import mbrl.replay_buffer as replay_buffer
@@ -16,35 +16,10 @@ import mbrl.types
 import mbrl.util as util
 
 MBPO_LOG_FORMAT = [
-    ("episode", "E", "int"),
-    ("step", "S", "int"),
+    ("epoch", "E", "int"),
+    ("env_step", "S", "int"),
     ("rollout_length", "RL", "int"),
-    ("train_dataset_size", "TD", "int"),
-    ("val_dataset_size", "VD", "int"),
-    ("model_loss", "MLOSS", "float"),
-    ("model_val_score", "MVSCORE", "float"),
-    ("model_best_val_score", "BMVSCORE", "float"),
-    ("sac_buffer_size", "SBSIZE", "int"),
-]
-
-EVAL_LOG_FORMAT = [
-    ("episode", "E", "int"),
-    ("step", "S", "int"),
-    ("episode_reward", "R", "float"),
-    ("model_reward", "MR", "float"),
-]
-
-SAC_TRAIN_LOG_FORMAT = [
-    ("episode", "E", "int"),
-    ("step", "S", "int"),
-    ("episode_reward", "R", "float"),
-    ("duration", "D", "time"),
-    ("batch_reward", "BR", "float"),
-    ("actor_loss", "ALOSS", "float"),
-    ("critic_loss", "CLOSS", "float"),
-    ("alpha_loss", "TLOSS", "float"),
-    ("alpha_value", "TVAL", "float"),
-    ("actor_entropy", "AENT", "float"),
+    ("eval_reward", "ER", "int"),
 ]
 
 
@@ -126,21 +101,9 @@ def train(
     agent = hydra.utils.instantiate(cfg.agent)
 
     work_dir = os.getcwd()
-    mbpo_logger = pytorch_sac.Logger(
-        work_dir,
-        save_tb=False,
-        log_frequency=cfg.log_frequency_model,
-        agent="model",
-        train_format=MBPO_LOG_FORMAT,
-        eval_format=EVAL_LOG_FORMAT,
-    )
-    sac_logger = pytorch_sac.Logger(
-        work_dir,
-        save_tb=False,
-        log_frequency=cfg.log_frequency_agent,
-        train_format=SAC_TRAIN_LOG_FORMAT,
-        eval_format=EVAL_LOG_FORMAT,
-    )
+    # enable_back_compatible to use pytorch_sac agent
+    logger = mbrl.logger.Logger(work_dir, enable_back_compatible=True)
+    logger.register_group("mbpo", MBPO_LOG_FORMAT, color="red", dump_frequency=1)
     video_recorder = pytorch_sac.VideoRecorder(work_dir if cfg.save_video else None)
 
     rng = np.random.default_rng(seed=cfg.seed)
@@ -172,7 +135,7 @@ def train(
         dynamics_model,
         env_dataset_train,
         dataset_val=env_dataset_val,
-        logger=mbpo_logger,
+        logger=logger,
         log_frequency=cfg.log_frequency_model,
     )
     best_eval_reward = -np.inf
@@ -180,7 +143,6 @@ def train(
     epoch = 0
     while epoch < cfg.overrides.num_trials:
         rollout_length = get_rollout_length(cfg.overrides.rollout_schedule, epoch)
-        mbpo_logger.log("train/rollout_length", rollout_length, 0)
 
         obs = env.reset()
         done = False
@@ -242,19 +204,25 @@ def train(
 
             # --------------- Agent Training -----------------
             for _ in range(cfg.overrides.num_sac_updates_per_step):
-                agent.update(sac_buffer, sac_logger, updates_made)
+                agent.update(sac_buffer, logger, updates_made)
                 updates_made += 1
                 if updates_made % cfg.log_frequency_agent == 0:
-                    sac_logger.dump(updates_made, save=True)
+                    logger.dump(updates_made, save=True)
 
             # ------ Epoch ended (evaluate and save model) ------
             if env_steps % cfg.overrides.trial_length == 0:
                 avg_reward = evaluate(
                     test_env, agent, cfg.algorithm.num_eval_episodes, video_recorder
                 )
-                mbpo_logger.log("eval/episode", epoch, env_steps)
-                mbpo_logger.log("eval/episode_reward", avg_reward, env_steps)
-                mbpo_logger.dump(env_steps, save=True)
+                logger.log_data(
+                    "mbpo",
+                    {
+                        "epoch": epoch,
+                        "env_step": env_steps,
+                        "eval_reward": avg_reward,
+                        "rollout_length": rollout_length,
+                    },
+                )
                 if avg_reward > best_eval_reward:
                     video_recorder.save(f"{epoch}.mp4")
                     best_eval_reward = avg_reward
