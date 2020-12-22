029e953966bc0f204766d6351fa3079124953fd6
Luis Pineda <luisen.p@gmail.com>
2020-12-22 09:06:09 -0500

    minor changes to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-import re
 from setuptools import setup, find_packages
+
 
 def parse_requirements_file(path):
     return [line.rstrip() for line in open(path, "r")]
+
 
 reqs_main = parse_requirements_file("requirements/main.txt")
 reqs_dev = parse_requirements_file("requirements/dev.txt")
@@ -14,11 +15,17 @@ with open("README.md", "r") as f:
 setup(
     name="mbrl_lib",
     version="0.0.1",
-    author="Luis Pineda",
+    author="Facebook AI Research",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/fairinternal/mbrl-lib",
     packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
     install_requires=reqs_main,
     extras_require={"dev": reqs_main + reqs_dev},
     include_package_data=True,
