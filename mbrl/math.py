c8b82a8340cba853db4ef058963f0cbaafdac6e0
Luis Pineda <luisen.p@gmail.com>
2021-02-15 15:22:12 -0500

    adjusted truncated_linear's constant case

--- mbrl/math.py
+++ mbrl/math.py
@@ -13,16 +13,22 @@ import mbrl.types
 def truncated_linear(
     min_x: float, max_x: float, min_y: float, max_y: float, x: float
 ) -> float:
+    """Truncated linear function.
+
+    Implements the following function:
+        f1(x) = min_y + (x - min_x) / (max_x - min_x) * (max_y - min_y)
+        f(x) = min(max_y, max(min_y, f1(x)))
+
+    If max_x - min_x < 1e-10, then it behaves as the constant f(x) = max_y
+    """
+    if max_x - min_x < 1e-10:
+        return max_y
     if x <= min_x:
         y: float = min_y
     else:
-        if max_x - min_x < 1e-6:
-            y = max_y
-        else:
-            dx = (x - min_x) / (max_x - min_x)
-            dx = min(dx, 1.0)
-            y = dx * (max_y - min_y) + min_y
-
+        dx = (x - min_x) / (max_x - min_x)
+        dx = min(dx, 1.0)
+        y = dx * (max_y - min_y) + min_y
     return y
 
 
