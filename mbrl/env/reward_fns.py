fixed bug in pets_pusher reward (wrong sign)

mbrl/env/reward_fns.py
@@ -35,4 +35,4 @@ def pusher(act: torch.Tensor, next_obs: torch.Tensor) -> torch.Tensor:
 
     act_cost = 0.1 * (act ** 2).sum(axis=1)
 
-    return obs_cost + act_cost
+    return -(obs_cost + act_cost)