7a0f3dd994a394bd071dcfbaa94e53727829e95f
Luis Pineda <luisen.p@gmail.com>
2021-03-08 13:13:28 -0500

    added test for create_replay_buffers

--- tests/core/test_common_utils.py
+++ tests/core/test_common_utils.py
@@ -2,6 +2,7 @@ import omegaconf
 import pytest
 
 import mbrl.models as models
+import mbrl.replay_buffer as replay_buffer
 import mbrl.util.common as utils
 
 
@@ -77,3 +78,55 @@ def test_create_dynamics_model():
     assert dynamics_model.num_elites == 8
     assert dynamics_model.no_delta_list == [0]
     assert dynamics_model.obs_process_fn == mock_obs_func
+
+
+def test_create_replay_buffers():
+    trial_length = 20
+    num_trials = 10
+    val_ratio = 0.1
+    cfg_dict = {
+        "dynamics_model": {"model": {"ensemble_size": 1}},
+        "algorithm": {},
+        "overrides": {
+            "trial_length": trial_length,
+            "num_trials": num_trials,
+            "model_batch_size": 64,
+            "validation_ratio": val_ratio,
+        },
+    }
+    cfg = omegaconf.OmegaConf.create(cfg_dict)
+    obs_shape = (6,)
+    act_shape = (4,)
+
+    def _check_shapes(train_cap):
+        val_cap = int(val_ratio * train_cap)
+        assert train.obs.shape == (train_cap, obs_shape[0])
+        assert val.obs.shape == (val_cap, obs_shape[0])
+        assert train.next_obs.shape == (train_cap, obs_shape[0])
+        assert val.next_obs.shape == (val_cap, obs_shape[0])
+        assert train.action.shape == (train_cap, act_shape[0])
+        assert val.action.shape == (val_cap, act_shape[0])
+        assert train.reward.shape == (train_cap,)
+        assert val.reward.shape == (val_cap,)
+        assert train.done.shape == (train_cap,)
+        assert val.done.shape == (val_cap,)
+
+    # Test reading from the above configuration and no bootstrap replay buffer
+    train, val = utils.create_replay_buffers(
+        cfg, obs_shape, act_shape, train_is_bootstrap=False
+    )
+    assert isinstance(train, replay_buffer.IterableReplayBuffer)
+    assert isinstance(val, replay_buffer.IterableReplayBuffer)
+
+    _check_shapes(num_trials * trial_length)
+
+    # Now add a training bootstrap and override the dataset size
+    cfg_dict["algorithm"]["dataset_size"] = 1500
+    cfg = omegaconf.OmegaConf.create(cfg_dict)
+    train, val = utils.create_replay_buffers(
+        cfg, obs_shape, act_shape, train_is_bootstrap=True
+    )
+    assert isinstance(train, replay_buffer.BootstrapReplayBuffer)
+    assert isinstance(val, replay_buffer.IterableReplayBuffer)
+
+    _check_shapes(1500)
