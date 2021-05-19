a6ddd50b71032bdabd815475883133e73669d148
Luis Pineda <luisen.p@gmail.com>
2021-05-19 08:46:23 -0400

    Added CNN encoder/decoder classes to models utils

--- mbrl/models/util.py
+++ mbrl/models/util.py
@@ -83,3 +83,81 @@ def to_tensor(x: mbrl.types.TensorType):
     if isinstance(x, np.ndarray):
         return torch.from_numpy(x)
     raise ValueError("Input must be torch.Tensor or np.ndarray.")
+
+
+class CNNEncoder(nn.Module):
+    def __init__(
+        self,
+        in_size: int,
+        feature_dim: int,
+        num_layers: int = 2,
+        num_filters: int = 32,
+    ):
+        super().__init__()
+        self.feature_dim = feature_dim
+        self.num_layers = num_layers
+
+        def _create_conv_layer(_in_size, _kernel_size, _stride):
+            return nn.Sequential(
+                nn.Conv2d(_in_size, num_filters, _kernel_size, stride=_stride),
+                nn.ReLU(),
+            )
+
+        self.convs = nn.ModuleList([_create_conv_layer(in_size, 3, 2)])
+        for i in range(num_layers - 1):
+            self.convs.append(_create_conv_layer(num_filters, 3, 1))
+
+        out_dim = 35
+        self.fc = nn.Linear(num_filters * out_dim * out_dim, self.feature_dim)
+        self.ln = nn.Sequential(nn.LayerNorm(self.feature_dim), nn.Tanh())
+
+    def forward(self, obs: torch.Tensor) -> torch.Tensor:
+        obs = obs / 255.0
+        conv = self.convs[0](obs)
+        for i in range(1, self.num_layers):
+            conv = self.convs[i](conv)
+        h = conv.view(conv.size(0), -1)
+        h_fc = self.fc(h)
+        return self.ln(h_fc)
+
+
+class CNNDecoder(nn.Module):
+    def __init__(
+        self,
+        out_size: int,
+        feature_dim: int,
+        num_layers: int = 2,
+        num_filters: int = 32,
+    ):
+        super().__init__()
+        self.num_layers = num_layers
+        self.num_filters = num_filters
+        self.out_dim = 35
+
+        self.fc = nn.Linear(feature_dim, num_filters * self.out_dim * self.out_dim)
+
+        def _create_deconv_layer(_out_size, _kernel_size, _stride, _output_padding=0):
+            return nn.Sequential(
+                nn.ConvTranspose2d(
+                    num_filters,
+                    _out_size,
+                    _kernel_size,
+                    stride=_stride,
+                    output_padding=_output_padding,
+                ),
+                nn.ReLU(),
+            )
+
+        self.deconvs = nn.ModuleList()
+
+        for i in range(self.num_layers - 1):
+            self.deconvs.append(_create_deconv_layer(num_filters, 3, 1))
+        self.deconvs.append(_create_deconv_layer(out_size, 3, 2, _output_padding=1))
+
+    def forward(self, h: torch.Tensor) -> torch.Tensor:
+        h = self.fc(h)
+        deconv = h.view(-1, self.num_filters, self.out_dim, self.out_dim)
+        for i in range(0, self.num_layers - 1):
+            deconv = self.deconvs[i](deconv)
+        obs = self.deconvs[-1](deconv)
+        return obs
