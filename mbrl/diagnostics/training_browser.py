93f116878948ac7b93b1eaab808e55273ba34740
Anis Ahmad <a3ahmad@gmail.com>
2021-05-19 01:17:30 -0700

    Add ability to change graph labels

--- mbrl/diagnostics/training_browser.py
+++ mbrl/diagnostics/training_browser.py
@@ -26,6 +26,8 @@ SOURCE = "results.csv"
 XCOL = "env_step"
 YCOL = "episode_reward"
 
+user_name_dict = {"x_label": "epochs", "y_label": "reward"}
+
 
 class ExperimentsModel(QAbstractTableModel):
     """
@@ -77,6 +79,69 @@ class ExperimentsModel(QAbstractTableModel):
         return None
 
 
+class GraphLabels(QAbstractTableModel):
+    """
+    Class to populate a table view with a pandas dataframe
+    """
+
+    def __init__(self, names, parent=None):
+        QAbstractTableModel.__init__(self, parent)
+        self.reloadExperiments(names)
+
+        self._headers = ["Algorithm", "Experiment", "Environment", "Seed"]
+
+    def reloadExperiments(self, names):
+        self.names = copy.deepcopy(names)
+        self.names = self.names + ["x_label", "y_label"]
+
+    def rowCount(self, parent=None):
+        return len(self.names)
+
+    def columnCount(self, parent=None):
+        return 1
+
+    def flags(self, index):
+        if not index.isValid():
+            return 0
+
+        return Qt.ItemIsEditable | Qt.ItemIsEnabled | Qt.ItemIsSelectable
+
+    def setData(self, index, value, role):
+        global user_name_dict
+
+        if role == Qt.EditRole:
+            row = index.row()
+            key = (
+                self.names[index.row()]
+                if (row < (self.rowCount() - 2))
+                else ("x_label" if (row < (self.rowCount() - 1)) else "y_label")
+            )
+            user_name_dict[key] = value
+
+            self.dataChanged.emit(index, index, [Qt.EditRole])
+
+            return True
+
+        return False
+
+    def data(self, index, role=Qt.DisplayRole):
+        global user_name_dict
+
+        if index.isValid():
+            if role == Qt.DisplayRole:
+                return str(user_name_dict[self.names[index.row()]])
+        return None
+
+    def headerData(self, row, orientation, role):
+        if orientation == Qt.Vertical and role == Qt.DisplayRole:
+            return (
+                f"Experiment #{row+1}"
+                if (row < (self.rowCount() - 2))
+                else ("X Label" if (row < (self.rowCount() - 1)) else "Y Label")
+            )
+        return None
+
+
 class BasicTrainingResultsWindow(QMainWindow):
     def __init__(self, experiment_root):
         super(BasicTrainingResultsWindow, self).__init__()
@@ -120,6 +185,16 @@ class BasicTrainingResultsWindow(QMainWindow):
         self.resultsWidget.setWidget(self.tableView)
         self.addDockWidget(Qt.LeftDockWidgetArea, self.resultsWidget)
 
+        self.graphLabelsWidget = QDockWidget("Graph Labels", self)
+        self.labelTable = GraphLabels(self.experiment_names, self)
+        self.labelView = QTableView()
+        self.labelView.setModel(self.labelTable)
+        self.graphLabelsWidget.setWidget(self.labelView)
+        self.addDockWidget(Qt.LeftDockWidgetArea, self.graphLabelsWidget)
+
+        self.tabifyDockWidget(self.graphLabelsWidget, self.resultsWidget)
+
+        self.labelTable.dataChanged.connect(self.onLabelChanged)
         self.displayAsDistributionCheckbox.stateChanged.connect(
             self.onChangeDisplayAsDistribution
         )
@@ -143,10 +218,17 @@ class BasicTrainingResultsWindow(QMainWindow):
 
         self.experiment_names = []
         for path in self.experiment_results:
-            name = path.replace(self.experiment_root, "").replace(
-                "/{}".format(SOURCE), ""
+            name = (
+                path.replace(self.experiment_root, "")
+                .replace("/{}".format(SOURCE), "")
+                .replace("/{}".format(MULTI_ROOT), "")
             )
             self.experiment_names.append(name)
+
+        global user_name_dict
+        for name in self.experiment_names:
+            if name not in user_name_dict:
+                user_name_dict[name] = name
 
     def selectedMatchingSequences(self):
         selection = self.tableView.selectionModel()
@@ -174,6 +256,9 @@ class BasicTrainingResultsWindow(QMainWindow):
 
         return False
 
+    def onLabelChanged(self, topLeft, bottomRight, roles):
+        self.onExperimentsSelectionChanged()
+
     def onSaveFigure(self, checked):
         fname, _ = QFileDialog.getSaveFileName(
             self, caption="Save figure", filter="Figures (*.png *.pdf)"
@@ -191,8 +276,11 @@ class BasicTrainingResultsWindow(QMainWindow):
         self.onExperimentsSelectionChanged()
 
     def onExperimentsSelectionChanged(self, sel1=None, sel2=None):
+        self.labelTable.reloadExperiments(self.experiment_names)
+
         selection = self.tableView.selectionModel()
 
+        global user_name_dict
         if selection.hasSelection():
             self.axes.cla()
 
@@ -227,7 +315,7 @@ class BasicTrainingResultsWindow(QMainWindow):
                     mean_line = self.axes.plot(
                         time_series,
                         mean_series,
-                        label=self.experiment_results[rowIndex],
+                        label=user_name_dict[self.experiment_names[rowIndex]],
                     )
                     self.axes.fill_between(
                         time_series,
@@ -239,6 +327,9 @@ class BasicTrainingResultsWindow(QMainWindow):
 
             if self.logYAxisCheckbox.checkState():
                 self.axes.semilogy()
+
+            self.axes.set_xlabel(user_name_dict["x_label"])
+            self.axes.set_ylabel(user_name_dict["y_label"])
 
             self.axes.legend()
             self.graphWidget.draw()
