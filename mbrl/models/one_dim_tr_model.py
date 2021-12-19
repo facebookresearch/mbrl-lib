[bug-fix] Fixed incorrect normalization in OneDTransitionRewardModel.sample() which was causing PETS to break in some problems.

mbrl/models/one_dim_tr_model.py
@@ -104,7 +104,7 @@ class OneDTransitionRewardModel(Model):
         self,
         obs: mbrl.types.TensorType,
         action: mbrl.types.TensorType,
-    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    ) -> torch.Tensor:
         if self.obs_process_fn:
             obs = self.obs_process_fn(obs)
         obs = model_util.to_tensor(obs).to(self.device)
@@ -113,7 +113,7 @@ class OneDTransitionRewardModel(Model):
         if self.input_normalizer:
             # Normalizer lives on device
             model_in = self.input_normalizer.normalize(model_in).float().to(self.device)
-        return model_in, obs.float(), action.float()
+        return model_in
 
     def _process_batch(
         self, batch: mbrl.types.TransitionBatch, _as_float: bool = False
@@ -127,7 +127,7 @@ class OneDTransitionRewardModel(Model):
             target_obs = next_obs
         target_obs = model_util.to_tensor(target_obs).to(self.device)
 
-        model_in, *_ = self._get_model_input(obs, action)
+        model_in = self._get_model_input(obs, action)
         if self.learned_rewards:
             reward = model_util.to_tensor(reward).to(self.device).unsqueeze(reward.ndim)
             target = torch.cat([target_obs, reward], dim=obs.ndim - 1)
@@ -269,7 +269,8 @@ class OneDTransitionRewardModel(Model):
         Returns:
             (tuple of two tensors): predicted next_observation (o_{t+1}) and rewards (r_{t+1}).
         """
-        model_in, obs, action = self._get_model_input(model_state["obs"], act)
+        obs = model_util.to_tensor(model_state["obs"]).to(self.device)
+        model_in = self._get_model_input(model_state["obs"], act)
         if not hasattr(self.model, "sample_1d"):
             raise RuntimeError(
                 "OneDTransitionRewardModel requires wrapped model to define method sample_1d"