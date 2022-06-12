03d383d84a7415ae2ff712e12b0b63f6f87f11c6
Rohan138 <rapotdar@purdue.edu>
2022-06-12 01:32:00 -0700

    drop deprecation test

--- mbrl/algorithms/planet.py
+++ mbrl/algorithms/planet.py
@@ -145,7 +145,8 @@ def train(
             dataset, num_epochs=1, batch_callback=batch_callback, evaluate=False
         )
         planet.save(work_dir)
-        replay_buffer.save(work_dir)
+        if cfg.overrides.get("save_replay_buffer", False):
+            replay_buffer.save(work_dir)
         metrics = get_metrics_and_clear_metric_containers()
         logger.log_data("metrics", metrics)
 
