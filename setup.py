3215e6eade397a2f40208345bb34bb4b3eb99c2f
Rohan138 <rapotdar@purdue.edu>
2022-08-02 23:31:30 -0700

    wip
    Signed-off-by: Rohan138 <rapotdar@purdue.edu>

--- setup.py
+++ setup.py
@@ -32,10 +32,6 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
