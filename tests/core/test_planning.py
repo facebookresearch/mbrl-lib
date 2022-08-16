ebd5751df7acd9d850b476741b384fbcd65a0090
Nathan Lambert <nol@berkeley.edu>
2022-08-16 15:49:05 -0700

    lint tests

--- tests/core/test_planning.py
+++ tests/core/test_planning.py
@@ -10,15 +10,19 @@ import mbrl.planning as planning
 
 SEED = 0
 
-def create_pid_agent(dim, state_mapping=None, batch_dim = 1):
+
+def create_pid_agent(dim,
+                     state_mapping=None,
+                     batch_dim=1):
     agent = planning.PIDAgent(k_p=np.random.randn(dim, ),
-                      k_i=np.random.randn(dim, ),
-                      k_d=np.random.randn(dim, ),
-                      target=np.zeros(dim, ),
-                      state_mapping=state_mapping,
+                              k_i=np.random.randn(dim, ),
+                              k_d=np.random.randn(dim, ),
+                              target=np.zeros(dim, ),
+                              state_mapping=state_mapping,
                               batch_dim=batch_dim,
-                      )
+                              )
     return agent
+
 
 def test_pid_agent_one_dim():
     """
@@ -39,13 +43,12 @@ def test_pid_agent_one_dim():
     assert np.sum(prev_error) == np.sum(cum_error) == 0
 
 
-
 def test_pid_agent_multi_dim():
     """
     This test covers regular updates for the multi-dim PID agent.
     """
     np.random.seed(SEED)
-    pid = create_pid_agent(dim=2, state_mapping = np.array([1, 3]),)
+    pid = create_pid_agent(dim=2, state_mapping=np.array([1, 3]), )
     init_obs = np.random.randn(4)
     act1 = pid.act(init_obs)
     next_obs = np.random.randn(4)
@@ -63,14 +66,14 @@ def test_pid_agent_batch(batch_dim=5):
     Tests the agent for batch-mode computation of actions.
     """
     np.random.seed(SEED)
-    pid = create_pid_agent(dim=2, state_mapping = np.array([1, 3]), batch_dim=batch_dim)
+    pid = create_pid_agent(dim=2, state_mapping=np.array([1, 3]), batch_dim=batch_dim)
 
     init_obs = np.random.randn(4, batch_dim)
     act1 = pid.act(init_obs)
     next_obs = np.random.randn(4, batch_dim)
     act2 = pid.act(next_obs)
 
-    assert (act1 + act2)[0] == pytest.approx([-7.155,  1.260,  8.679, -0.047, -1.962], 0.1)
+    assert (act1 + act2)[0] == pytest.approx([-7.155, 1.260, 8.679, -0.047, -1.962], 0.1)
 
     # check reset
     pid.reset()
