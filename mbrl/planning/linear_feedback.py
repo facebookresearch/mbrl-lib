4b251385837648d2dcf411f4a0d1330558f175d2
Nathan Lambert <nol@berkeley.edu>
2022-08-16 15:47:28 -0700

    make batch friendly, add tests

--- mbrl/planning/linear_feedback.py
+++ mbrl/planning/linear_feedback.py
@@ -2,7 +2,6 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-
 from typing import Optional
 
 import numpy as np
@@ -18,43 +17,45 @@ class PIDAgent(Agent):
     https://en.wikipedia.org/wiki/PID_controller.
 
     Args:
-        dim (int): dimensionality of state and control signal
-        k_p (np.ndarry): proportional control coeff
-        k_i (np.ndarry): integral control coeff
-        k_d (np.ndarry): derivative control coeff
-        target (np.ndarry): setpoint
+        k_p (np.ndarry): proportional control coeff (Nx1)
+        k_i (np.ndarry): integral control coeff     (Nx1)
+        k_d (np.ndarry): derivative control coeff   (Nx1)
+        target (np.ndarry): setpoint                (Nx1)
         state_mapping (np.ndarry): indices of the state vector to apply the PID control to.
             E.g. for a system with states [angle, angle_vel, position, position_vel], state_mapping
             of [1, 3] and dim of 2 will apply the PID to angle_vel and position_vel variables.
+        batch_dim (int): number of samples to compute actions for simultaneously
     """
 
     def __init__(
         self,
-        dim: int,
         k_p: np.ndarray,
         k_i: np.ndarray,
         k_d: np.ndarray,
         target: np.ndarray,
         state_mapping: Optional[np.ndarray] = None,
+        batch_dim: Optional[int] = 1,
     ):
         super().__init__()
-        assert (
-            len(k_p) == len(k_i) == len(k_d) == len(target) == len(state_mapping) == dim
-        )
-        self.n_dof = dim
+        assert len(k_p) == len(k_i) == len(k_d) == len(target)
+        self.n_dof = len(k_p)
 
         # State mapping defaults to first N states
         if state_mapping is not None:
+            assert len(state_mapping) == len(target)
             self.state_mapping = state_mapping
         else:
-            self.state_mapping = np.arange(0, dim)
+            self.state_mapping = np.arange(0, self.n_dof)
 
-        self.k_p = k_p
-        self.k_i = k_i
-        self.k_d = k_d
-        self.target = target
-        self.prev_error = 0
-        self.cum_error = 0
+        self.batch_dim = batch_dim
+
+        self._prev_error = np.zeros((self.n_dof, self.batch_dim))
+        self._cum_error = np.zeros((self.n_dof, self.batch_dim))
+
+        self.k_p = np.repeat(k_p[:, np.newaxis], self.batch_dim, axis=1)
+        self.k_i = np.repeat(k_i[:, np.newaxis], self.batch_dim, axis=1)
+        self.k_d = np.repeat(k_d[:, np.newaxis], self.batch_dim, axis=1)
+        self.target = np.repeat(target[:, np.newaxis], self.batch_dim, axis=1)
 
     def act(self, obs: np.ndarray, **_kwargs) -> np.ndarray:
         """Issues an action given an observation.
@@ -70,20 +71,32 @@ class PIDAgent(Agent):
         Returns:
             (np.ndarray): the action.
         """
+        if obs.ndim == 1:
+            obs = np.expand_dims(obs, -1)
         if len(obs) > self.n_dof:
-            obs = obs[: self.n_dof]
-        q_des = self.target
-        q = obs
+            pos = obs[self.state_mapping]
+        else:
+            pos = obs
 
-        error = q_des - q
-        self.cum_error += error
+        error = self.target - pos
+        self._cum_error += error
 
-        P_value = self.k_p * error
-        I_value = self.k_i * self.cum_error
-        D_value = self.k_d * (error - self.prev_error)
-        self.prev_error = error
+        P_value = np.multiply(self.k_p, error)
+        I_value = np.multiply(self.k_i, self._cum_error)
+        D_value = np.multiply(self.k_d, (error - self._prev_error))
+        self._prev_error = error
         action = P_value + I_value + D_value
         return action
+
+    def reset(self):
+        """
+        Reset internal errors.
+        """
+        self._prev_error = np.zeros((self.n_dof, self.batch_dim))
+        self._cum_error = np.zeros((self.n_dof, self.batch_dim))
+
+    def get_errors(self):
+        return self._prev_error, self._cum_error
 
     def _get_P(self):
         return self.k_p
---
+++ tests/core/test_planning.py
@@ -0,0 +1,78 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import numpy as np
+import pytest
+
+import mbrl.planning as planning
+
+SEED = 0
+
+def create_pid_agent(dim, state_mapping=None, batch_dim = 1):
+    agent = planning.PIDAgent(k_p=np.random.randn(dim, ),
+                      k_i=np.random.randn(dim, ),
+                      k_d=np.random.randn(dim, ),
+                      target=np.zeros(dim, ),
+                      state_mapping=state_mapping,
+                              batch_dim=batch_dim,
+                      )
+    return agent
+
+def test_pid_agent_one_dim():
+    """
+    This test covers the creation of PID agents in the most basic form.
+    """
+    np.random.seed(SEED)
+    pid = create_pid_agent(dim=1)
+    pid.reset()
+    init_obs = np.random.randn(1)
+    act = pid.act(init_obs)
+
+    # check action computation
+    assert act == pytest.approx(-7.043, 0.1)
+
+    # check reset
+    pid.reset()
+    prev_error, cum_error = pid.get_errors()
+    assert np.sum(prev_error) == np.sum(cum_error) == 0
+
+
+
+def test_pid_agent_multi_dim():
+    """
+    This test covers regular updates for the multi-dim PID agent.
+    """
+    np.random.seed(SEED)
+    pid = create_pid_agent(dim=2, state_mapping = np.array([1, 3]),)
+    init_obs = np.random.randn(4)
+    act1 = pid.act(init_obs)
+    next_obs = np.random.randn(4)
+    act2 = pid.act(next_obs)
+    assert act1 + act2 == pytest.approx([-6.141, -2.207], 0.1)
+
+    # check reset
+    pid.reset()
+    prev_error, cum_error = pid.get_errors()
+    assert np.sum(prev_error) == np.sum(cum_error) == 0
+
+
+def test_pid_agent_batch(batch_dim=5):
+    """
+    Tests the agent for batch-mode computation of actions.
+    """
+    np.random.seed(SEED)
+    pid = create_pid_agent(dim=2, state_mapping = np.array([1, 3]), batch_dim=batch_dim)
+
+    init_obs = np.random.randn(4, batch_dim)
+    act1 = pid.act(init_obs)
+    next_obs = np.random.randn(4, batch_dim)
+    act2 = pid.act(next_obs)
+
+    assert (act1 + act2)[0] == pytest.approx([-7.155,  1.260,  8.679, -0.047, -1.962], 0.1)
+
+    # check reset
+    pid.reset()
+    prev_error, cum_error = pid.get_errors()
+    assert np.sum(prev_error) == np.sum(cum_error) == 0
