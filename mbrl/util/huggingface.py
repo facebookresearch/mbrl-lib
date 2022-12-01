5c72e651e271e521f2142ee2ab3a01a70caebc09
Nathan Lambert <nol@berkeley.edu>
2022-12-01 14:53:56 -0800

    remove unused code

--- mbrl/util/huggingface.py
+++ mbrl/util/huggingface.py
@@ -1,31 +1,24 @@
-import datetime
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 import json
 import os
 import shutil
 import tempfile
 import zipfile
 from pathlib import Path
-from typing import Any, Dict, Optional, Tuple, Union
+from typing import Any, Dict, Optional, Tuple
 
 import gym
-import numpy as np
-import stable_baselines3
 from huggingface_hub import HfApi, upload_folder
 from huggingface_hub.repocard import metadata_eval_result, metadata_save
-from stable_baselines3.common.base_class import BaseAlgorithm
-from stable_baselines3.common.evaluation import evaluate_policy
-from stable_baselines3.common.vec_env import (
-    DummyVecEnv,
-    VecEnv,
-    VecVideoRecorder,
-    unwrap_vec_normalize,
-)
-from wasabi import Printer
 
-msg = Printer()
+from ..models import Model
 
 
-def _generate_config(model: BaseAlgorithm, local_path: Path) -> None:
+def _generate_config(model: Model, local_path: Path) -> None:
     """
     Generate a config.json file containing information
     about the agent and the environment
@@ -46,52 +39,11 @@ def _generate_config(model: BaseAlgorithm, local_path: Path) -> None:
     with open(Path.joinpath(local_path, unzipped_model_folder, "data")) as json_file:
         data = json.load(json_file)
         # Add system_info elements to our JSON
-        data["system_info"] = stable_baselines3.get_system_info(print_info=False)[0]
+        # data["system_info"] = stable_baselines3.get_system_info(print_info=False)[0]
 
     # Step 3: Write our config.json file
     with open(local_path / "config.json", "w") as outfile:
         json.dump(data, outfile)
-
-
-def _evaluate_agent(
-    model: BaseAlgorithm,
-    eval_env: VecEnv,
-    n_eval_episodes: int,
-    is_deterministic: bool,
-    local_path: Path,
-) -> Tuple[float, float]:
-    """
-    Evaluate the agent using SB3 evaluate_policy method
-    and create a results.json
-    :param model: name of the model object
-    :param eval_env: environment used to evaluate the agent
-    :param n_eval_episodes: number of evaluation episodes
-    :param is_deterministic: use deterministic or stochastic actions
-    :param local_path: path of the local repository
-    """
-    # Step 1: Evaluate the agent
-    mean_reward, std_reward = evaluate_policy(
-        model, eval_env, n_eval_episodes, is_deterministic
-    )
-
-    # Step 2: Create json evaluation
-    # First get datetime
-    eval_datetime = datetime.datetime.now()
-    eval_form_datetime = eval_datetime.isoformat()
-
-    evaluate_data = {
-        "mean_reward": mean_reward,
-        "std_reward": std_reward,
-        "is_deterministic": is_deterministic,
-        "n_eval_episodes": n_eval_episodes,
-        "eval_datetime": eval_form_datetime,
-    }
-
-    # Step 3: Write a JSON file
-    with open(local_path / "results.json", "w") as outfile:
-        json.dump(evaluate_data, outfile)
-
-    return mean_reward, std_reward
 
 
 def is_atari(env_id: str) -> bool:
@@ -103,70 +55,6 @@ def is_atari(env_id: str) -> bool:
     entry_point = gym.envs.registry.env_specs[env_id].entry_point
     return "AtariEnv" in str(entry_point)
 
-
-def _generate_replay(
-    model: BaseAlgorithm,
-    eval_env: VecEnv,
-    video_length: int,
-    is_deterministic: bool,
-    local_path: Path,
-):
-    """
-    Generate a replay video of the agent
-    :param model: trained model
-    :param eval_env: environment used to evaluate the agent
-    :param video_length: length of the video (in timesteps)
-    :param is_deterministic: use deterministic or stochastic actions
-    :param local_path: path of the local repository
-    """
-    # This is another temporary directory for video outputs
-    # SB3 created a -step-0-to-... meta files as well as other
-    # artifacts which we don't want in the repo.
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        # Step 1: Create the VecVideoRecorder
-        env = VecVideoRecorder(
-            eval_env,
-            tmpdirname,
-            record_video_trigger=lambda x: x == 0,
-            video_length=video_length,
-            name_prefix="",
-        )
-
-        obs = env.reset()
-        lstm_states = None
-        episode_starts = np.ones((env.num_envs,), dtype=bool)
-
-        try:
-            for _ in range(video_length + 1):
-                action, lstm_states = model.predict(
-                    obs,
-                    state=lstm_states,
-                    episode_start=episode_starts,
-                    deterministic=is_deterministic,
-                )
-                obs, _, episode_starts, _ = env.step(action)
-
-            # Save the video
-            env.close()
-
-            # Convert the video with x264 codec
-            inp = env.video_recorder.path
-            out = os.path.join(local_path, "replay.mp4")
-            os.system(f"ffmpeg -y -i {inp} -vcodec h264 {out}".format(inp, out))
-
-        except KeyboardInterrupt:
-            pass
-        except Exception as e:
-            msg.fail(str(e))
-            # Add a message for video
-            msg.fail(
-                "We are unable to generate a replay of your agent, "
-                "the package_to_hub process continues"
-            )
-            msg.fail(
-                "Please open an issue at "
-                "https://github.com/huggingface/huggingface_sb3/issues"
-            )
 
 
 def generate_metadata(
@@ -280,11 +168,11 @@ def _add_logdir(local_path: Path, logdir: Path):
 
 
 def package_to_hub(
-    model: BaseAlgorithm,
+    model: Model,
     model_name: str,
     model_architecture: str,
     env_id: str,
-    eval_env: Union[VecEnv, gym.Env],
+    eval_env: gym.Env,
     repo_id: str,
     commit_message: str,
     is_deterministic: bool = True,
@@ -292,6 +180,8 @@ def package_to_hub(
     token: Optional[str] = None,
     video_length=1000,
     logs=None,
+    mean_reward=None,
+    std_reward=None,
 ):
     """
     Evaluate, Generate a video and Upload a model to Hugging Face Hub.
@@ -314,16 +204,12 @@ def package_to_hub(
     :param logs: directory on local machine of tensorboard logs you'd like to upload
     """
 
-    # Autowrap, so we only have VecEnv afterward
-    if not isinstance(eval_env, VecEnv):
-        eval_env = DummyVecEnv([lambda: eval_env])
-
-    msg.info(
-        "This function will save, evaluate, generate a video of your agent, "
+    msg = (
+        "This function will save your agent, "
         "create a model card and push everything to the hub. "
-        "It might take up to 1min. \n "
         "This is a work in progress: if you encounter a bug, please open an issue."
     )
+    print(msg)
 
     repo_url = HfApi().create_repo(
         repo_id=repo_id,
@@ -340,15 +226,15 @@ def package_to_hub(
 
         # Retrieve VecNormalize wrapper if it exists
         # we need to save the statistics
-        maybe_vec_normalize = unwrap_vec_normalize(eval_env)
-
-        # Save the normalization
-        if maybe_vec_normalize is not None:
-            maybe_vec_normalize.save(tmpdirname / "vec_normalize.pkl")
-            # Do not update the stats at test time
-            maybe_vec_normalize.training = False
-            # Reward normalization is not needed at test time
-            maybe_vec_normalize.norm_reward = False
+        # maybe_vec_normalize = unwrap_vec_normalize(eval_env)
+        #
+        # # Save the normalization
+        # if maybe_vec_normalize is not None:
+        #     maybe_vec_normalize.save(tmpdirname / "vec_normalize.pkl")
+        #     # Do not update the stats at test time
+        #     maybe_vec_normalize.training = False
+        #     # Reward normalization is not needed at test time
+        #     maybe_vec_normalize.norm_reward = False
 
         # We create two versions of the environment:
         # one for video generation and one for evaluation
@@ -361,15 +247,7 @@ def package_to_hub(
         # Step 2: Create a config file
         _generate_config(model_name, tmpdirname)
 
-        # Step 3: Evaluate the agent
-        mean_reward, std_reward = _evaluate_agent(
-            model, eval_env, n_eval_episodes, is_deterministic, tmpdirname
-        )
-
-        # Step 4: Generate a video
-        _generate_replay(model, replay_env, video_length, is_deterministic, tmpdirname)
-
-        # Step 5: Generate the model card
+        # Step 3: Generate the model card
         generated_model_card, metadata = _generate_model_card(
             model_architecture, env_id, mean_reward, std_reward
         )
@@ -433,7 +311,7 @@ def push_to_hub(
         _copy_file(Path(filename_path), tmpdirname)
         _save_model_card(tmpdirname, "", {})
 
-        msg.info(f"Pushing repo {repo_id} to the Hugging Face Hub")
+        print(f"Pushing repo {repo_id} to the Hugging Face Hub")
         repo_url = upload_folder(
             repo_id=repo_id,
             folder_path=tmpdirname,
@@ -442,7 +320,7 @@ def push_to_hub(
             token=token,
         )
 
-    msg.good(
+    print(
         f"Your model has been uploaded to the Hub, you can find it here: {repo_url}"
     )
     return repo_url
